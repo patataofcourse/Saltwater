c87e860551e8c6e95e4086caf544e7d00e710d4b
patataofcourse <patataofcourse@gmail.com>
2022-02-02 01:18:53 +0100

    memory leak bad

--- src/Megamix/RHMPatch.cpp
+++ src/Megamix/RHMPatch.cpp
@@ -10,9 +10,8 @@ namespace Megamix {
     char* rhmpatchBuffer = new char[0x200000]; //Should be 2MB
 
     int LoadC00Bin(const std::string &path="_:/saffron/C00.bin") {
-        File* file = new File();
-        File::Open(*file, path, File::Mode::READ);
-        u32 result = file->Read(rhmpatchBuffer, 0x200000);
+        File file(path, File::Mode::READ);
+        u32 result = file.Read(rhmpatchBuffer, 0x200000);
         return result;
     }
 
