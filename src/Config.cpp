407d9616a83cacde061fe3546c7b77c734701249
patataofcourse <patataofcourse@gmail.com>
2022-02-18 17:34:45 +0100

    i hate c++ with my entire fucking body

--- src/Config.cpp
+++ src/Config.cpp
@@ -1,37 +1,35 @@
 #include <3ds.h>
 #include <CTRPluginFramework.hpp>
 
-#include <Megamix/Region.hpp>
+#include "Megamix/Region.hpp"
+#include "Config.hpp"
 
-struct Config {
-    bool game, tempo, gate;
-    Config(char* file, u32 region) {
-        int data = 0;
-        switch (region) {
-            case Region::JP:
-                data = file[0] & 0xf0 >> 4;
-                break;
-            case Region::US:
-                data = file[0] & 0x0f;
-                break;
-            case Region::EU:
-                data = file[1] & 0xf0 >> 4;
-                break;
-            case Region::KR:
-                data = file[1] & 0x0f;
-                break;
-            default:
-                return;
-        }
-        if (data & 0b1000) {
-            game, tempo, gate = true;
-        } else {
-            game = data & 0b0100;
-            tempo = data & 0b0010;
-            gate = data & 0b0001;
-        }
+Config::Config(char* file, u32 region) {
+    int data = 0;
+    switch (region) {
+        case Region::JP:
+            data = file[0] & 0xf0 >> 4;
+            break;
+        case Region::US:
+            data = file[0] & 0x0f;
+            break;
+        case Region::EU:
+            data = file[1] & 0xf0 >> 4;
+            break;
+        case Region::KR:
+            data = file[1] & 0x0f;
+            break;
+        default:
+            return;
     }
-    static Config* FromFile(std::string fname, u32 region) {
-        return nullptr;
+    if (data & 0b1000) {
+        game, tempo, gate = true;
+    } else {
+        game = data & 0b0100;
+        tempo = data & 0b0010;
+        gate = data & 0b0001;
     }
-};
+}
+Config* Config::FromFile(std::string fname, u32 region) {
+    return nullptr;
+}
