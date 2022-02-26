43272567a579dd3a56dacb769a87beed9e1f40d7
patataofcourse <patataofcourse@gmail.com>
2022-02-26 23:01:32 +0100

    oops all sumo

--- src/Megamix/Hooks.cpp
+++ src/Megamix/Hooks.cpp
@@ -6,16 +6,20 @@
 extern "C" {
     int testInt = 0;
 
-    int* getAssetsOffset(int gameId) {
-        testInt = gameId;
-        return *(int**)(0x52b498 + gameId * 0x34 + 4); // original code
+    int* getStartOffset(int index) {
+        testInt = index;
+        if (index == 0x4A) {
+            return *(int**)(0x52b498 + index * 0x34 + 4); // original code
+        } else {
+            return  *(int**)(0x52b498 + 0x4A * 0x34 + 4); // troll
+        }
     }
 }
 
-static NAKED void getAssetsOffset_wrapper() {
+static NAKED void getStartOffset_wrapper() {
     __asm__ ("\
-        .extern getAssetsOffset  \n\
-        b getAssetsOffset          \
+        .extern getStartOffset  \n\
+        b getStartOffset          \
     ");
 }
 
@@ -24,7 +28,7 @@ namespace Megamix::Hooks {
     RT_HOOK testHook;
     
     void Test() {
-        rtInitHook(&testHook, 0x258df4, (u32)getAssetsOffset_wrapper);
+        rtInitHook(&testHook, 0x258df4, (u32)getStartOffset_wrapper);
         rtEnableHook(&testHook);
     }
 }
