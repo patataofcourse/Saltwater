ce03268f49f3501d28fb4831f38abe43c4b0835b
patataofcourse <patataofcourse@gmail.com>
2022-03-02 15:22:54 +0100

    Finish BTKS header

--- src/Megamix/BTKS.cpp
+++ src/Megamix/BTKS.cpp
@@ -13,15 +13,28 @@ namespace Megamix {
         u32* intBuf = new u32[1];
 
         //Header
-        result = file.Read(magicBuf, 4);
+        result = file.Read(magicBuf, 4); // Magic
         if (!result) return result;
-
         if (magicBuf != "BTKS")
             return -6; // Not a BTKS file
         
-        file.Read(intBuf, 4); //filesize
+        result = file.Read(intBuf, 4); // Filesize - not that useful but eh
+        if (!result) return result;
+        int filesize = *intBuf;
 
-        //TODO: continue
+        result = file.Read(intBuf, 4); // Format version - supported: rev0
+        if (!result) return result;
+        int version = *intBuf;
+        if (version != 0)
+            return -7; // Unsupported version
+        
+        // Since the only supported version is rev0 we can expect it to always be that
+
+        result = file.Read(intBuf, 4); // Number of sections - expected: 3 or 4
+        if (!result) return result;
+        int numSections = *intBuf;
+
+        // Section code! This is where things get more complicated
 
         return result;
     }
