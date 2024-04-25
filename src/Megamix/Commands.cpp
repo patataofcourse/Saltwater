0fe1a9a62816024b8bfd563df5f7116f7795eea1
TheAlternateDoctor <thealternatedoc@gmail.com>
2024-04-25 11:15:09 +0200

    Added used attribute to compile properly

--- src/Megamix/Commands.cpp
+++ src/Megamix/Commands.cpp
@@ -4,7 +4,8 @@
 #include "Megamix.hpp"
 
 namespace Megamix{
-extern "C" void tickflowCommandsHook(CTickflow* self, u32 cmd_num, u32 arg0, u32* args){
+
+    extern "C" __attribute__((used)) void tickflowCommandsHook(CTickflow* self, u32 cmd_num, u32 arg0, u32* args){
         if (cmd_num == VersionNumber){ //Returns version number
             if (arg0 == 0){ //RHMPatch version
                 self->mCondvar = 0x103;
@@ -13,7 +14,6 @@ extern "C" void tickflowCommandsHook(CTickflow* self, u32 cmd_num, u32 arg0, u32
             }
         }
     }
-
 
     void tickflowCommandsHookWrapper() {
         asm(
